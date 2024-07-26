febad5e80bee19e3d2ece0018fa529f5bf6c4ad2
harishgorakainfo <gharish1903@gmail.com>
2024-07-26 14:53:04 +0530

    first data 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['www.synthochirals.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ["www.synthochirals.com"],
+  },
+};
+export default nextConfig;
