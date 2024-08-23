7a1d3330dca790b82fa1c8e7d0e9166d645c77a2
harishgorakainfo <gharish1903@gmail.com>
2024-08-23 16:36:13 +0530

    tenth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Navbar from "./component/Navbar";
-// import Footer from "./component/Footer";
 import Style from "./style/responsive.css";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {/* <Navbar /> */}
-        {children}
-
-        {/* <Footer /> */}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
