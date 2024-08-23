80cebfa800d3698de8e119b0aa67251f0bc59000
harishgorakainfo <gharish1903@gmail.com>
2024-08-23 17:06:27 +0530

    class

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,16 @@
+// postcss.config.cjs
+const postcssPresetEnv = require("postcss-preset-env");
+const cssnano = require("cssnano");
+
 module.exports = {
   plugins: [
-    require("postcss-preset-env"),
-    require("cssnano")({
+    postcssPresetEnv(),
+    cssnano({
       preset: [
         "default",
         {
           discardComments: { removeAll: true },
           minifyFontValues: false,
-          // Disable some other optimizations
-          // Other options can be added here as needed
         },
       ],
     }),
